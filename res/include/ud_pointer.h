74515d64bfcc4aa1452e222f8f8588d25508b17e
Timothée DAUTREME <tim.dautreme@hotmail.fr>
2019-10-15 16:40:26 +0200

    ptr set macro update

--- res/include/ud_pointer.h
+++ res/include/ud_pointer.h
@@ -7,16 +7,17 @@
 #include <ud_memory.h>
 
 // Macro
-# define    ud_ptr_set(type, ...)   ({ type *ptr; type in_val[] = {__VA_ARGS__}; size_t len = sizeof(in_val) / sizeof(type *); ptr = ud_ptr_init(type, len); type *p_ptr = ptr; for (ud_ut_count i = 0; i < len; ++i, ++p_ptr) *p_ptr = in_val[i]; (type *)ptr; })
+# define    ud_ptr_set(type, ...) ({ type ptr; type in_val[] = {__VA_ARGS__}; type *in_val_tmp = in_val; size_t len = sizeof(in_val) / sizeof(type); ptr = ud_ptr_init(len); type p_ptr = ptr; for (ud_ut_count i = 0; i < len; ++i) *p_ptr++ = *in_val_tmp++; ptr; })
 /*
 # define    ud_ptr_set(type, ...) \
     ({ \
         type ptr; \
         type in_val[] = {__VA_ARGS__}; \
+        type *in_val_tmp = in_val; \
         size_t len = sizeof(in_val) / sizeof(type); \
         ptr = ud_ptr_init(len); \
         type p_ptr = ptr; \
-        for (ud_ut_count i = 0; i < len; ++i, ++p_ptr) *p_ptr = in_val[i]; \
+        for (ud_ut_count i = 0; i < len; ++i) *p_ptr++ = *in_val_tmp++; \
         ptr; \
     })
 */
