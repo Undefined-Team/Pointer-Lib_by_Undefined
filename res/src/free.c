313124698495f4d4b8642232eaf654056b1ed828
Timothée DAUTREME <tim.dautreme@hotmail.fr>
2019-10-21 21:41:35 +0200

    fix ptr free

--- res/src/free.c
+++ res/src/free.c
@@ -2,6 +2,6 @@
 
 void    ud_ptr_free_ctr(void **ptr, size_t depth)
 {
-    if (depth-- > 0) for (void **ptr_tmp = ptr; ptr_tmp;) ud_ptr_free(*ptr_tmp++, depth);
+    if (depth-- > 0) for (void **ptr_tmp = ptr; *ptr_tmp;) ud_ptr_free(*ptr_tmp++, depth);
     ud_ut_free(ptr);
 }
