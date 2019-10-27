8f8dddbd357e49107823d215274ee70c2230ec1a
sbedene <sylvainbedene@gmail.com>
2019-10-27 15:30:21 +0100

    unit tests

--- unit test/test.c
+++ unit test/test.c
@@ -9,19 +9,11 @@ int main(void)
 
     char **t = ud_ptr_init(char *, 4);
     char ***ptr = ud_ptr_set(char **, test, test2, test3);
-    printf("%zd\n", ud_ptr_len(ptr));
     char ***ptr2 = ud_ptr_cpy(ptr);
-    printf("%zd\n", ud_ptr_len(ptr2));
-    for (char i = 0; i < 3; ++i) printf("ptr1 %s\n", ptr[i][0]);
-    for (char i = 0; i < 3; ++i) printf("ptr2 %s\n", ptr2[i][0]);
 
     // char *test_fe[] = {"slt", "cava", NULL};
     char *test_fe[] = {"slt", "cava", NULL};
     // ud_ptr_foreach(test_fe);
-    ud_ptr_foreach(test_fe, elem, 
-    printf("str = %s\n", *elem);
-    printf("st = %s\n", *elem);
-    );
     // printf("str = %s\n", *key);
 
     // size_t *test_malloc = malloc(sizeof(test_malloc) * 27);
@@ -36,6 +28,10 @@ int main(void)
     ud_ut_test(!ud_mem_cmp(ptr2[0][1], "allo", 4));
     ud_ut_test(!ud_mem_cmp(ptr2[1][0], "test", 4));
     ud_ut_test(!ud_mem_cmp(ptr2[2][0], "test", 4));
+    ud_ut_test(!ud_mem_cmp(ptr[0], ptr2[0], sizeof(void *)));
+    ud_ut_test(!ud_mem_cmp(ptr[1], ptr2[1], sizeof(void *)));
+    ud_ut_test(!ud_mem_cmp(ptr[2], ptr2[2], sizeof(void *)));
+    ud_ut_test(!ud_mem_cmp(*ptr, *ptr2, ud_ptr_len(ptr)));
     ud_ut_test(ptr != ptr2);
     ud_ut_test(ud_ptr_len(ptr) == 3);
     ud_ut_test(ud_ptr_len(ptr2) == 3);
